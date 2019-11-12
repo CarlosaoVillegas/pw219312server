b980f9b30e8b713999c0cbbb782b11e5af958090
CarlosaoVillegas <carlos.a.o.villegas@gmail.com>
2019-11-12 12:53:05 -0700

    Node 2

--- crud-mysql-ejemplo/src/app.js
+++ crud-mysql-ejemplo/src/app.js
@@ -1,17 +1,20 @@
-const path = require();
+const path = require('path');
 const express = require('express');
 const mysql = require ('mysql');
 const myConnection = require ('express-myconnection');
 const app = express();
 //importar rutas
-const indice
+const indiceRutas = require('./rutas/index');
 //Configuraciones 
 app.set('port',process.env.PORT || 3000);
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'vistas'))
 
 //Middleware
-app.use()
+//app.use()
+//usamos las rutas 
+app.use('/',indiceRutas);
+
 
 app.listen(app.get('port'),() => {
 	console.log('Esuchando en el puerto 3000');
